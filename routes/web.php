68f845ee511f0727c8007bdf13075fb0c0b0af20
matty_smart <mattysmart1234@gmail.com>
2021-04-05 07:00:09 -0800

    add some tests for projects

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use App\Http\Controllers\ProjectController;
 |
 */
 
+Route::get('/', function () {return view('welcome');});
+
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/dashboard', function () {
         return view('dashboard');
---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Project;
+use App\Models\User;
+
+class ProjectsTest extends TestCase
+{
+    use withFaker, RefreshDatabase;
+
+    /** @test */
+    public function logged_in_user_can_see_projects()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $projects = Project::factory()->count(5)->create();
+
+        $response = $this->get('/projects')->assertSuccessful();
+
+        foreach ($projects as $project) {
+            $response->assertSeeText($project->title);
+        }
+    }
+
+    /** @test */
+    public function logged_in_user_can_view_a_project()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $project = Project::factory()->create();
+
+        $this->get('/projects/'.$project->id)->assertSuccessful()->assertSeeText($project->title);
+    }
+
+    /** @test */
+    public function logged_in_user_can_create_a_project()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $attributes = ['user_id'=>$user->id,'title'=>$this->faker->title,'description'=>$this->faker->sentence];
+
+        $this->post('/projects',$attributes);
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+
+    /** @test */
+    public function logged_in_user_can_update_a_project()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $attributes = ['user_id'=>$user->id,'title'=>$this->faker->title,'description'=>$this->faker->sentence];
+
+        $this->post('/projects',$attributes);
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+}
