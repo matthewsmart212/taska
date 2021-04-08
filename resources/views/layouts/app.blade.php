b93ae558c63b670f5866e854fc6a13c5b4392917
matty_smart <mattysmart1234@gmail.com>
2021-04-08 06:49:23 -0800

    only admin can see users tab on navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,9 @@
                     <li class="mb-4 mt-4 block"><a href="/dashboard"><i class="fas fa-home mr-2"></i> Dashboard</a></li>
                     <li class="mb-4 mt-4 block"><a href="/projects"><i class="fas fa-tasks mr-2"></i> Projects</a></li>
                     <li class="mb-4 mt-4 block"><a href="/teams"><i class="fas fa-user-friends mr-2"></i> Teams</a></li>
-                    <li class="mb-4 mt-4 block"><a href="/users"><i class="fas fa-user mr-2"></i> users</a></li>
+                    @if(auth()->user()->isAdmin())
+                        <li class="mb-4 mt-4 block"><a href="/users"><i class="fas fa-user mr-2"></i> Users</a></li>
+                    @endif
                 </ul>
 
             </aside>
