f9fa772e38bfd43f0061e31957d3526f81081a8b
matty_smart <mattysmart1234@gmail.com>
2021-04-08 04:40:08 -0800

    add timestamps to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -19,24 +20,32 @@ class UserSeeder extends Seeder
             'name' => 'Matthew Smart',
             'email' => 'matthew.smart212@gmail.com',
             'password' => Hash::make('password'),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
 
         DB::table('users')->insert([
             'name' => 'John Doe',
             'email' => 'john.doe@gmail.com',
             'password' => Hash::make('password'),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
 
         DB::table('users')->insert([
             'name' => 'Jane Doe',
             'email' => 'jane.doe@gmail.com',
             'password' => Hash::make('password'),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
 
         DB::table('users')->insert([
             'name' => 'Bob Doe',
             'email' => 'bob.doe@gmail.com',
             'password' => Hash::make('password'),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
     }
 }
