8bff1865e938b4552ef7c720c1088458a398f244
matty_smart <mattysmart1234@gmail.com>
2021-04-15 12:14:34 -0800

    only admin can see create new project button

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -12,7 +12,7 @@
 <main class="bg-gray-100 pt-20 px-7" style="margin-top:56px;min-height: calc(100vh - 57px); background-image:url('/images/backgrounds/2.jpg');background-size:cover;" >
     @include('modals.create-project')
     <div class="grid grid-cols-7 gap-5">
-            @foreach($projects as $project)
+            @forelse($projects as $project)
                 <div
                     class="p-6 pl-0 pt-12 rounded-lg"
                      style="height:120px; background-image:url('{{ $project->background_image }}');background-size:cover;">
@@ -20,16 +20,25 @@
                     <a class="block mb-1 text-white pl-6 bg-gray-700 bg-opacity-75 rounded-tr-md rounded-br-md"
                         href="{{ $project->path() }}"> {{ $project->title }}
                     </a>
-
                 </div>
-            @endforeach
-            <div
-                class="bg-gray-200 bg-opacity-75 p-6 rounded-lg text-center cursor-pointer hover:bg-gray-700 hover:text-white"
-                onclick="toggleModal('modal-id')"
-                style="min-height:120px;"
-            >
-                <p class="inline-block mt-6">Create new project</p>
-            </div>
+            @empty
+                <div
+                    class="bg-gray-200 bg-opacity-75 p-6 rounded-lg text-center cursor-pointer hover:bg-gray-700 hover:text-white"
+                    style="min-height:120px;"
+                >
+                    <p class="inline-block mt-6">No Projects Available</p>
+                </div>
+            @endforelse
+
+            @if(auth()->user()->isAdmin())
+                <div
+                    class="bg-gray-200 bg-opacity-75 p-6 rounded-lg text-center cursor-pointer hover:bg-gray-700 hover:text-white"
+                    onclick="toggleModal('modal-id')"
+                    style="min-height:120px;"
+                >
+                    <p class="inline-block mt-6">Create new project</p>
+                </div>
+            @endif
     </div>
 </main>
 </x-app-layout>
