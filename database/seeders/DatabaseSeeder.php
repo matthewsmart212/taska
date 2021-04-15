11ed759f9ec7c0837bf3920c0a39066f92d294e3
matty_smart <mattysmart1234@gmail.com>
2021-04-15 09:45:04 -0800

    remove UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
             ProjectSeeder::class,
             GroupSeeder::class,
             TaskSeeder::class,
